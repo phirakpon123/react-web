78b8649d5a4975566f413178475090a09a5cc740
khontharos <khontharos.bb@gmail.com>
2020-07-19 14:50:38 +0700

    Updates

---
+++ precache-manifest.a0c51effb4fc9d8b5eb58d1f65369b95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d70dcd3d2bb8bb89e66060d325efc42",
+    "url": "/react-web/index.html"
+  },
+  {
+    "revision": "e9d82f7aa1daef29a4f7",
+    "url": "/react-web/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ab19bf8c19133784f41b",
+    "url": "/react-web/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "e9d82f7aa1daef29a4f7",
+    "url": "/react-web/static/js/2.708ad432.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-web/static/js/2.708ad432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab19bf8c19133784f41b",
+    "url": "/react-web/static/js/main.e9d38a41.chunk.js"
+  },
+  {
+    "revision": "063a4a56ff8e19105526",
+    "url": "/react-web/static/js/runtime-main.e0253e73.js"
+  }
+]);
